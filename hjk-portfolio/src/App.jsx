a385214552b10855a242ea766a372c378a17e0bf
Hayat <hayatjahangir0@gmail.com>
2025-02-14 20:02:43 +0530

    download all dependencies and also some settings for this project according to project requrired

--- hjk-portfolio/src/App.jsx
+++ hjk-portfolio/src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <main>
       <h1 className='text-red-500 bg-slate-300 flex justify-center'>Hayat Jahangir khan</h1>
-    </>
+    </main>
   )
 }
 
