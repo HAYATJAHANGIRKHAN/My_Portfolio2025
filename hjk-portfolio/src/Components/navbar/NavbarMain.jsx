f9c5bf9da637db3340d5883c19fdb444caf282a8
Hayat <hayatjahangir0@gmail.com>
2025-02-15 19:48:21 +0530

    update

---
+++ hjk-portfolio/src/Components/navbar/NavbarMain.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import NavbarLogo from './NavbarLogo'
+import NavbarLinks from './NavbarLinks'
+import NavbarBtn from './NavbarBtn'
+
+
+
+function NavbarMain() {
+  return (
+    <nav>
+    <NavbarLogo/>
+    <NavbarLinks/>
+    <NavbarBtn/>
+
+
+      
+    </nav>
+  )
+}
+
+export default NavbarMain
